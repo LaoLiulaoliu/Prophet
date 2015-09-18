0cc86fbe97b470759bafb9dee2fea6c5290ad3ef
xijing <dxj19831029@gmail.com>
2015-09-18 13:45:47 +0800

    disable apt-get install, and enable pip upgrade and ansi_seg clone

--- install_env.sh
+++ install_env.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
 
-sudo apt-get install libatlas-base-dev gfortran
+#sudo apt-get install libatlas-base-dev gfortran
 
 Bin="env/bin"
 
 if ! test -d "$Bin" ; then
-	virtualenv env
+	virtualenv -p /usr/bin/python env
 fi
 
+${Bin}/pip install --upgrade pip
 ${Bin}/pip install Cython
 ${Bin}/pip install scipy==0.15.1
 ${Bin}/pip install gensim
+
+
+if ! test -d "library" ; then
+	git clone https://github.com/NLPchina/ansj_seg.git
+	cp -r ansj_seg/library ./
+fi
+
