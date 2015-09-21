de64cc9bb4220adf1b45289b3b3276b337b98bab
xijing <dxj19831029@gmail.com>
2015-09-21 19:12:14 +0800

    give 0.5 on dropout and increase ppl's dim

--- prophet/ppl_context_model.py
+++ prophet/ppl_context_model.py
@@ -9,21 +9,21 @@ from six.moves import cPickle
 import os, re, json
 
 from keras.layers.embeddings import Embedding
-from keras.layers.core import Dense, Flatten
+from keras.layers.core import Dense, Flatten, Dropout
 from keras.models import Sequential 
 from keras.optimizers import SGD, RMSprop, Adagrad
 from keras.utils import np_utils, generic_utils
 from keras.regularizers import l2
 from prophet.weibo_reader import WeiboReader
 from prophet.metric import WeiboPrecision
-from prophet.common import weibo_loss, weibo_loss_weighted, weibo_loss_scaled_weighted
+from prophet.common import weibo_loss, weibo_loss_weighted, weibo_loss_scaled_weighted, build_percision_funcs, WeiboPrecisionCallback, build_precisio_stack
 from keras.callbacks import ModelCheckpoint
 from prophet.ppl_idx_table import PplIdxTable
 
 max_features = 1000000  # vocabulary size: top 50,000 most common words in data
 skip_top = 0  # ignore top 100 most common words
 nb_epoch = 1
-dim_proj = 10  # embedding space dimension
+dim_proj = 300  # embedding space dimension
 dim_output = 3 # the output predicting values.
 
 save = True
@@ -77,6 +77,7 @@ max_features = idx_table.get_table_idx()
 print('Build model...')
 model = Sequential()
 model.add(Embedding(max_features, dim_proj, init="uniform"))
+model.add(Dropout(0.5))
 model.add(Flatten())
 model.add(Dense(dim_proj, dim_output, init="uniform", activation="linear",
                 W_regularizer=l2(0.01)))
@@ -85,14 +86,12 @@ sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
 #sgd = SGD(lr=2, decay=1e-6, momentum=0.9, nesterov=True)
 #use weibo_loss_weighted with learning rate 2 is good.
 #model.compile(loss=weibo_loss_weighted, optimizer=sgd)
-model.compile(loss=weibo_loss_scaled_weighted, optimizer=sgd)
+model.compile(loss=weibo_loss_scaled_weighted, optimizer=sgd, other_func_init=build_precisio_stack)
 
 checkpoint = ModelCheckpoint(save_dir+"/ppl_context_state.full_t.10.f10.pkl", save_best_only=False)
-model.fit(train_ppl, train_gt, batch_size=256, nb_epoch=100, show_accuracy=True, callbacks=[checkpoint])
-train_pre=model.predict(train_ppl, batch_size=128)
-print("traing weibo acc: ", WeiboPrecision.precision_match(train_gt, train_pre))
+precision = WeiboPrecisionCallback()
+model.fit(train_ppl, train_gt, batch_size=256, nb_epoch=120, show_accuracy=True, callbacks=[checkpoint, precision], validation_data=(val_ppl, val_gt))
 
-print("validation acc: ", model.evaluate(val_ppl, val_gt, 128, show_accuracy=True))
 pre=model.predict(val_ppl, batch_size=128)
 print("validation weibo acc: ", WeiboPrecision.precision_match(val_gt, pre))
 
