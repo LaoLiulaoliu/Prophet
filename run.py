7fde4691637655f8a0d2f4161b8eee6f26424198
xijing <dxj19831029@gmail.com>
2015-09-22 12:52:41 +0800

    print some general info

--- run.py
+++ run.py
@@ -3,18 +3,39 @@
 
 
 import prophet
-
-#reader = prophet.WeiboReader()
-#reader.load_data("./data/weibo_train_data.txt")
-#reader.print_dataset_info()
-#reader.print_top_list()
+training_filename="./data/weibo_train_data.txt"
+predict_filename="./data/weibo_predict_data.txt"
 
 reader = prophet.WeiboReader()
-reader.load_data("./data/weibo_predict_data.txt")
-data_list = reader.data()
-predictions = []
-for data in data_list:
-  predictions.append((0,0,0))
-reader.save_data(predictions, "./result.txt")
+reader.load_data(training_filename)
+print("========================================================")
+reader.print_dataset_info()
+print("========================================================")
+reader.print_top_list()
+print("========================================================")
+reader.print_rank_info()
+print("========================================================")
+from prophet.ppl_idx_table import PplIdxTable
+train_data = reader.get_training_data()
+idx_table = PplIdxTable()
+idx_table.create_ppls_table(train_data, lambda info: info[0])
+val_data = reader.get_validation_data()
+idx_table.reset_missing()
+idx_table.get_ppls_idx(val_data, lambda info: info[0])
+print("----validation missing: %d users" % (idx_table.get_missing_uniq_ppl()) )
+idx_table.reset_missing()
+rd2 = prophet.WeiboReader()
+rd2.load_data(predict_filename)
+predict_data = rd2._data
+idx_table.get_ppls_idx(predict_data, lambda info: info[0])
+print("-- predict data missing %d users" % idx_table.get_missing_uniq_ppl())
+
+#reader = prophet.WeiboReader()
+#reader.load_data("./data/weibo_predict_data.txt")
+#data_list = reader.data()
+#predictions = []
+#for data in data_list:
+#  predictions.append((0,0,0))
+#reader.save_data(predictions, "./result.txt")
 #reader.load_data("./data/weibo_predict_data.txt")
 #reader.print_dataset_info()
