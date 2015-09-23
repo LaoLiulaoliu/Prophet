b6e516ab26363a33dd54957cd842507c5b86d832
xijing <dxj19831029@gmail.com>
2015-09-23 11:30:01 +0800

    add models and combine model

---
+++ prophet/combine_model.py
@@ -0,0 +1,67 @@
+# vim: set fileencoding=utf-8 :
+#!/usr/bin/env python
+from __future__ import absolute_import
+from __future__ import print_function
+
+import os
+
+from keras.optimizers import SGD, RMSprop, Adagrad
+from keras.callbacks import ModelCheckpoint
+from prophet.common import weibo_loss, weibo_loss_weighted, weibo_loss_scaled_weighted, WeiboPrecisionCallback, build_precisio_stack
+
+from prophet.data import WeiboDataset
+from prophet.models import *
+
+dim=100
+    
+save_dir = "gen_nn_model"
+if not os.path.exists(save_dir):
+    os.makedirs(save_dir)
+    
+
+dataset = WeiboDataset()
+print('Loading the data...')
+dataset.load_data(
+             ("./data2/weibo_train_data.txt", "./gen_data2/weibo_train_data_text.txt.jian.words"),
+             ("./data2/weibo_predict_data.txt", "./gen_data2/weibo_predict_data_text.txt.jian.words")
+             )
+print('Loading word vector model')
+word_vec_filename="./gen_model2/vec_state_s100_p1_w5_t1.all"
+phrase_filenames="./gen_model2/vec_state_s100_p1_w5_t1.all.phrase0"
+dataset.load_words_vec(word_vec_filename, phrase_filenames, max_len=10)
+
+print('Generating training/validation/predicting data')
+train_gt = dataset.get_training_data_gt_np()
+val_gt = dataset.get_validation_data_gt_np()
+print("The max len of words is: ", dataset.get_missing_info(is_valid=False, is_predict=False, is_max_len=True, is_print=False))
+
+train_words = dataset.get_words_vec_training_data_np()
+train_ppl = dataset.get_ppl_training_data_np()
+val_words = dataset.get_words_vec_validation_data_np()
+val_ppl = dataset.get_ppl_validation_data_np()
+predict_words = dataset.get_words_vec_predict_data_np()
+predict_ppl = dataset.get_ppl_predict_data_np()
+
+print('Building the model')
+vec_dim=100
+model = build_combine_model(dataset.get_ppl_max_count(), vec_dim=vec_dim)
+
+sgd = SGD(lr=0.1, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss=weibo_loss_scaled_weighted, optimizer=sgd, other_func_init=build_precisio_stack)
+#model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+
+print("Traing the model")
+checkpoint = ModelCheckpoint(save_dir+"/combine_state.full_t.pkl", save_best_only=False)
+precision = WeiboPrecisionCallback()
+model.fit([train_ppl, train_words], train_gt, batch_size=256, nb_epoch=121, show_accuracy=True, callbacks=[checkpoint, precision], validation_data=([val_ppl, val_words], val_gt))
+train_res = model.predict([train_ppl, train_words], batch_size=128)
+val_res = model.predict([val_ppl, val_words], batch_size=128)
+import numpy
+dataset._train_reader.save_data(numpy.append(train_res, val_res, 0), "./combine_val_result.txt")
+
+
+print("predict shape: ", predict_words.shape)
+pre=model.predict([predict_ppl, predict_words], batch_size=128)
+dataset.save_predictions(pre, './combine_result.txt')
+exit(1)
+
---
+++ prophet/models.py
@@ -0,0 +1,87 @@
+# vim: set fileencoding=utf-8 :
+#!/usr/bin/env python
+
+from keras.layers.embeddings import Embedding
+from keras.layers.core import Dense, Flatten, Dropout, Merge
+from keras.models import Sequential 
+from keras.optimizers import SGD, RMSprop, Adagrad
+from keras.regularizers import l2
+from keras.layers.recurrent import LSTM
+import os
+
+
+def build_ppl_context_model(max_ppl, dim_proj=300, dim_output=3, saved_filename = None, is_output=True):
+  model = Sequential()
+  model.add(Embedding(max_ppl, dim_proj, init="uniform"))
+  model.add(Dropout(0.7))
+  model.add(Flatten())
+  if is_output:
+    model.add(Dense(dim_proj, dim_output, init="uniform", activation="linear",
+                  W_regularizer=l2(0.01)))
+  #else:
+  #  model.add(Dense(dim_proj, 1024, init="uniform", activation="linear",
+  #                W_regularizer=l2(0.01)))
+  if saved_filename is not None and os.path.isfile(saved_filename):
+    model.load_weights(saved_filename)
+  return model
+
+
+def build_content_context_model_2_lstm(in_dim=100, hidden1=512, hidden2=512, 
+        dense1=1024, dim_output=3, drop1=0.5, drop2=0.6, 
+        saved_filename = None, is_output=True):
+  model = Sequential()
+  model.add(LSTM(in_dim, hidden1, return_sequences=True))  # try using a GRU instead, for fun
+  model.add(Dropout(drop1))
+  model.add(LSTM(hidden1, hidden2, return_sequences=False))
+  model.add(Dropout(drop2))
+  model.add(Dense(hidden2, dense1, init='uniform', activation="linear",
+                  W_regularizer=l2(0.01)))
+  if is_output:
+    model.add(Dense(dense1, dim_output, init="uniform", activation="linear",
+                  W_regularizer=l2(0.01)))
+  if saved_filename is not None and os.path.isfile(saved_filename):
+    model.load_weights(saved_filename)
+  return model
+
+def build_content_contex_model_lstm(in_dim=100, hidden1=512, dense1=1024, 
+                                    dim_output=3, drop1=0.5,
+                                    saved_filename=None, is_output=True):
+  model = Sequential()
+  model.add(LSTM(in_dim, hidden1, return_sequences=False))  # try using a GRU instead, for fun
+  model.add(Dropout(drop1))
+  model.add(Dense(hidden1, dense1, init='uniform', activation="linear",
+                  W_regularizer=l2(0.01)))
+  if is_output:
+    model.add(Dense(dense1, dim_output, init="uniform", activation="linear",
+                  W_regularizer=l2(0.01)))
+  if saved_filename is not None and os.path.isfile(saved_filename):
+    model.load_weights(saved_filename)
+  return model
+
+def build_combine_model(max_ppl, dim_proj=300, vec_dim=100, 
+                        lstm_hidden=256, dim_output=3, 
+                        saved_filename=None, is_output=True):
+  ppl_model = build_ppl_context_model(max_ppl, dim_proj, 
+                            dim_output, is_output=False)
+
+  dense1=1024  
+  content_model = build_content_contex_model_lstm(vec_dim, 
+                              lstm_hidden, 
+                              dim_output=dim_output, 
+                              dense1=dense1,
+                              drop1=0.5, 
+                              saved_filename=None, 
+                              is_output=False)
+  model=Sequential()
+  model.add(Merge([ppl_model, content_model], mode='concat', concat_axis=1))
+  #model.add(Dense(dim_proj+dense1, 1024, activation='linear',
+  #                W_regularizer=l2(0.01)))
+  if is_output:
+    model.add(Dense(dim_proj+dense1, dim_output, init="uniform", activation="linear",
+                  W_regularizer=l2(0.01)))
+
+  if saved_filename is not None and os.path.isfile(saved_filename):
+    model.load_weights(saved_filename)
+        
+  return model
+
