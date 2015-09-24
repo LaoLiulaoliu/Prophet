25fa2560adfa6eef715f2d507d68f7a5a18e11f8
xijing <dxj19831029@gmail.com>
2015-09-24 18:20:22 +0800

    use data matrix mode, which won't use that much memory

--- prophet/conv2d_model.py
+++ prophet/conv2d_model.py
@@ -23,7 +23,7 @@ if not os.path.exists(save_dir):
     os.makedirs(save_dir)
     
 print("Training conv2d model")
-dataset = WeiboDataset(10000)
+dataset = WeiboDataset()
 print('Loading the data...')
 dataset.load_data(
              ("./data2/weibo_train_data.txt", "./gen_data2/weibo_train_data_text.txt.jian.words"),
@@ -33,29 +33,29 @@ print('Loading word vector model')
 word_vec_filename="./gen_model2/vec_state_s100_p0_w5_t1.all"
 #phrase_filenames="./gen_model2/vec_state_s100_p1_w5_t1.all.phrase0"
 phrase_filenames=None
-dataset.load_words_vec(word_vec_filename, phrase_filenames, max_len=10)
+dataset.load_words_vec(word_vec_filename, phrase_filenames, max_len=None)
 
 print('Generating training/validation/predicting data')
-train_gt = dataset.get_training_data_gt_np()
-val_gt = dataset.get_validation_data_gt_np()
+train_gt = dataset.get_training_data_gt_matrix()
+val_gt = dataset.get_validation_data_gt_matrix()
 print("The max len of words is: ", dataset.get_missing_info(is_valid=False, is_predict=False, is_max_len=True, is_print=False))
 
-train_words = dataset.get_words_vec_training_data_np(is_conv_2d=True)
+train_words = dataset.get_wrods_vec_training_data_matrix(is_conv_2d=True)
 #train_ppl = dataset.get_ppl_training_data_np()
-val_words = dataset.get_words_vec_validation_data_np(is_conv_2d=True)
+val_words = dataset.get_words_vec_validation_data_matrix(is_conv_2d=True)
 #val_ppl = dataset.get_ppl_validation_data_np()
-predict_words = dataset.get_words_vec_predict_data_np(is_conv_2d=True)
+predict_words = dataset.get_words_vec_predict_data_matrix(is_conv_2d=True)
 #predict_ppl = dataset.get_ppl_predict_data_np()
-#import numpy as np
-#if np.isnan(train_words).any():
-##  print("train words contains nan")
-#  exit(1)
-#if np.isnan(val_words).any():
-##  print("val_words contains nan")
-#  exit(1)
-#if np.isnan(predict_words):
-#  print("predict_words contains nan")
-#  exit(1)
+# import numpy as np
+# if np.isnan(train_words).any():
+#   print("train words contains nan")
+#   exit(1)
+# if np.isnan(val_words).any():
+#   print("val_words contains nan")
+#   exit(1)
+# if np.isnan(predict_words).any():
+#   print("predict_words contains nan")
+#   exit(1)
   
 print('Building the model')
 vec_dim=100
@@ -70,7 +70,8 @@ model.compile(loss=weibo_loss_scaled_weighted, optimizer=sgd, other_func_init=bu
 print("Traing the model")
 checkpoint = ModelCheckpoint(save_dir+"/combine_state.full_t.pkl", save_best_only=False)
 precision = WeiboPrecisionCallback()
-model.fit(train_words, train_gt, batch_size=256, nb_epoch=121, show_accuracy=True, callbacks=[checkpoint, precision], validation_data=( val_words, val_gt))
+print(train_words.shape)
+model.fit(train_words, train_gt, batch_size=256, nb_epoch=121, show_accuracy=True, callbacks=[checkpoint, precision], validation_data=( val_words, val_gt), shuffle=True)
 train_res = model.predict(train_words, batch_size=128)
 val_res = model.predict(val_words, batch_size=128)
 import numpy
